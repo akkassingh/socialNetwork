52969d147d0102fbbfee8f4767b8d79ae4d84af2
Akkas Singh <akkassingh@Akkass-MacBook-Air.local>
2019-05-16 22:49:35 +0530

    Frontend login and logout

---
+++ client/src/App.js
@@ -0,0 +1,40 @@
+import React,{Fragment, useEffect} from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/layouts/Navbar';
+import Landing from './components/layouts/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Alert from './components/layouts/Alert';
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+import './App.css';
+import setAuthToken from './utils/setAuthToken';
+
+if(localStorage.token){
+    setAuthToken(localStorage.token)
+}
+
+const App= () => {
+    useEffect(() => {
+    store.dispatch(loadUser())
+    },[]);
+    return(
+    <Provider store={store}>
+        <Router>
+        <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className="container">
+        <Alert />
+        <Switch>
+        <Route exact path = "/register" component = { Register } />
+        <Route exact path = "/login" component = { Login } />
+        </Switch>
+        </section>
+        </Fragment>
+        </Router>
+    </Provider>
+)};
+export default App;
