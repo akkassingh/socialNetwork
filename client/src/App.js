9ac7babfc6895cb348642a66c544cad8d891e98d
Akkas Singh <akkassingh@Akkass-MacBook-Air.local>
2019-05-23 02:24:50 +0530

    loader + create profile page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React,{Fragment, useEffect} from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Navbar from './components/layouts/Navbar';
-import Landing from './components/layouts/Landing';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
-import Alert from './components/layouts/Alert';
+import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
 //Redux
