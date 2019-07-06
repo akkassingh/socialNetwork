bef3abd746d3ad0f269c3fdc70a6a812e61e6962
Akkas Singh <akkassingh@Akkass-MacBook-Air.local>
2019-07-06 18:52:31 +0530

    changes or heroku deployment.

--- server.js
+++ server.js
@@ -27,6 +27,8 @@ if(process.env.NODE_ENV=== 'production'){
     })
 }
 
-const PORT = process.env.PORT || 5000;
+app.set( 'port', ( process.env.PORT || 5000 ));
 
-app.listen(PORT, () => console.log(`server listening on Port ${PORT}`));
+app.listen( app.get( 'port' ), function() {
+    console.log( 'Server is running on port ' + app.get( 'port' ));
+    });
